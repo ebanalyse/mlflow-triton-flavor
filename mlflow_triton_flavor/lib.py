1416e6f4fec7675aa416adbfbd08eb0d332aac76
Julian <julian.neeleman@eb.dk>
2024-10-14 14:22:29 +0200

    Simplify arguments

--- mlflow_triton_flavor/lib.py
+++ mlflow_triton_flavor/lib.py
@@ -6,63 +6,52 @@ from mlflow.exceptions import MlflowException
 from mlflow.models import Model
 from mlflow.models.model import MLMODEL_FILE_NAME, ModelInfo
 from mlflow.protos.databricks_pb2 import RESOURCE_ALREADY_EXISTS
-from mlflow.tracking._model_registry import DEFAULT_AWAIT_MAX_SLEEP_SECONDS
 
 FLAVOR_NAME = "triton"
 
 
-def save_model(triton_model_path, path, mlflow_model=Model()):
+def save_model(source_path: str, dest_path: str, mlflow_model=Model()):
     """
     Save a Triton model to a path on the local file system.
 
-    :param triton_model_path: File path to Triton model to be saved.
-    :param path: Local path where the model is to be saved.
+    :param source_path: Path to Triton model folder.
+    :param dest_path: Path to where the model should be saved.
     :param mlflow_model: :py:mod:`mlflow.models.Model` this flavor is being added to.
 
     """
 
-    path = os.path.abspath(path)
-    if os.path.exists(path):
+    dest_path = os.path.abspath(dest_path)
+    if os.path.exists(dest_path):
         raise MlflowException(
-            message="Path '{}' already exists".format(path),
+            message="Path '{}' already exists".format(dest_path),
             error_code=RESOURCE_ALREADY_EXISTS,
         )
-    os.makedirs(path)
-    triton_model_path = os.path.normpath(triton_model_path)
-    model_data_subpath = os.path.basename(triton_model_path)
-    model_data_path = os.path.join(path, model_data_subpath)
+    os.makedirs(dest_path)
 
-    # Save Triton model
-    shutil.copytree(triton_model_path, model_data_path)
+    source_path = os.path.normpath(source_path)
+    model_data_subpath = os.path.basename(source_path)
+    model_data_path = os.path.join(dest_path, model_data_subpath)
+
+    shutil.copytree(source_path, model_data_path)
 
     mlflow_model.add_flavor(FLAVOR_NAME, data=model_data_subpath)
-    mlflow_model.save(os.path.join(path, MLMODEL_FILE_NAME))
+    mlflow_model.save(os.path.join(dest_path, MLMODEL_FILE_NAME))
 
 
-def log_model(
-    triton_model_path,
-    artifact_path,
-    registered_model_name=None,
-    await_registration_for=DEFAULT_AWAIT_MAX_SLEEP_SECONDS,
-) -> ModelInfo:
+def log_model(path: str, register_model: bool = True) -> ModelInfo:
     """
     Log a Triton model as an MLflow artifact for the current run.
 
-    :param triton_model_path: File path to Triton model.
-    :param artifact_path: Run-relative artifact path.
-    :param registered_model_name: (Experimental) If given, create a model version under
-                                  ``registered_model_name``, also creating a registered model if one
-                                  with the given name does not exist.
-
-    :param await_registration_for: Number of seconds to wait for the model version to finish
-                            being created and is in ``READY`` status. By default, the function
-                            waits for five minutes. Specify 0 or None to skip waiting.
+    :param path: Path to Triton model folder.
+    :param register_model: If given, create a model version, also creating a registered
+                            model if one with the given name does not exist. The name is
+                            equal to the basename of the ``path``.
 
     """
+    model_name = os.path.basename(os.path.normpath(path))
     return Model.log(
-        artifact_path=artifact_path,
+        artifact_path=model_name,
         flavor=sys.modules[__name__],
-        triton_model_path=triton_model_path,
-        registered_model_name=registered_model_name,
-        await_registration_for=await_registration_for,
+        triton_model_path=path,
+        registered_model_name=model_name if register_model else None,
     )
